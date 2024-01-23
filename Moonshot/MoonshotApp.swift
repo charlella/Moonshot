bf561cd428d71e1cb0b0effd5224aec8cf9af678
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-01-23 11:11:43 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by charlene hoareau on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
